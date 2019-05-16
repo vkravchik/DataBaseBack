29824ca796bc2fbce300acdc8133eb10f01112dc
Vadim <vkravchik@gmail.com>
2019-05-16 11:18:53 +0300

    change table user

--- src/main/java/com/spring/db/model/User.java
+++ src/main/java/com/spring/db/model/User.java
@@ -1,10 +1,8 @@
 package com.spring.db.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Table(name = "usr")
 @Entity
 public class User {
     @Id
